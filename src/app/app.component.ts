c977f18b15e2c8d4c86a5919cbb2765b5475205e
bahadurmirza1 <mirzabahadurlatif@gmail.com>
2024-09-29 13:41:22 +0500

    new commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CustomTableComponent } from "./custom-table/custom-table.component";
+import { FormComponent } from "./form/form.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CustomTableComponent, FormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-app';
+  title = 'localstorage_TableCRUD';
 }
